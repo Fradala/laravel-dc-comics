996ccff5116e40892df98af24e695cb9b673e295
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2024-02-05 15:29:53 +0100

    mod

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController as GuestComicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+
+Route::get('/comics', [GuestComicController::class, 'index']);
